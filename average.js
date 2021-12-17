d23b5d0f2ddf805ed2ce7c791189c12ce2029195
Mihir Thakur <tamihir27@yahoo.com>
2021-12-17 18:27:47 +0530

    Updated average.js

--- average.js
+++ average.js
@@ -1,7 +1,11 @@
 
 function average(numbers) {
-  filteredNums = numbers.filter((num) => if !Number.isNaN(num) return num);
-  if(!filteredNums.length) return NaN;
+  filteredNums = numbers.filter((num) => {
+    if(!Number.isNaN(num)) return num
+  });
+  if(!filteredNums.length){
+    return NaN;
+  }
   return filteredNums.reduce((p, c)=> p+c, 0) / numbers.length;
 }
 
