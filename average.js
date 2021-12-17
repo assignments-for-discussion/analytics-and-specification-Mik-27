e19dec8b425a6beaea695f836ff42a92c59698cd
Mihir Thakur <tamihir27@yahoo.com>
2021-12-17 18:11:07 +0530

    Updated average.js
    Conditions for 'NaN' data value.

--- average.js
+++ average.js
@@ -1,6 +1,10 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  return numbers.reduce((p, c)=> {
+    if(!p) p=0;
+    if(!c) c=0;
+    return p+c
+  }, 0) / numbers.length;
 }
 
 module.exports = {average};
